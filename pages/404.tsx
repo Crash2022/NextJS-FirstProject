2a69fd431981c963a074300a10b754062114260d
Crash2022 <crash555@mail.ru>
2023-02-03 11:05:51 +0500

    add: redirect from 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,20 @@
 import Link from 'next/link';
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 const NotFoundPage = () => {
+
+    const router = useRouter()
+
+    useEffect(() => {
+        const timeoutID = setTimeout(() => {
+            router.push('/').then()
+        }, 5000)
+
+        return () => {
+            clearTimeout(timeoutID)
+        }
+    }, [])
 
     return (
         <div className="notFound">
@@ -8,10 +22,11 @@ const NotFoundPage = () => {
                 Ooops...! Error 404: Page Not Found
             </div>
             <div className="notFound_text">
-                <span>Go to </span>
+                <span>You will be redirect automatically to </span>
                 <Link href='/'>
                     Home Page
                 </Link>
+                <span> after 3 seconds...</span>
             </div>
             <div>
                 <img src="/images/error404.jpg" alt="page404"/>
